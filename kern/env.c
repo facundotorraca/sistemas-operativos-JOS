861693f809b94c677f062aa7403eda5ddb974d90
nicofandos <nfandos@fi.uba.ar>
2020-07-03 23:03:23 -0300

    Tarea 1 (codigo) realizada

--- kern/env.c
+++ kern/env.c
@@ -112,8 +112,7 @@ void
 env_init(void)
 {
 	// Set up envs array
-    size_t i;
-    for (i = NENV - 1; i >= 0; i--) {
+    for (int i = NENV - 1; i >= 0; i--) {
         envs[i].env_id = 0;
         envs[i].env_link = env_free_list;
         env_free_list = &envs[i];
