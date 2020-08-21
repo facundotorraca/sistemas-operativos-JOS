4d867b108251cc0db841098d15e93512f1319670
nicofandos <nfandos@fi.uba.ar>
2020-08-20 22:18:39 -0300

    Primera tarea parte 4

--- kern/syscall.c
+++ kern/syscall.c
@@ -139,7 +139,22 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+
+    struct Env *e;
+    int r = envid2env(envid, &e, 1);
+    if (r < 0)
+        return r;
+    if ((uintptr_t)tf >= UTOP)
+        panic("Syscall set trapframe trapframe address is bigger than UTOP");
+    if ((tf->tf_cs & 3) != 3)
+        panic("Syscall set trapframe wrong CPL (ring level is not 3)");
+    //Assign trapframe to environment
+    e->env_tf = *tf;
+    //Enable interrupts
+    e->env_tf.tf_eflags |= FL_IF;
+    //Set IOPL to 0
+    e->env_tf.tf_eflags &= ~FL_IOPL_MASK;
+    return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -423,6 +438,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
             return sys_ipc_try_send(a1, a2, (void *)a3, a4);
 	    case SYS_env_set_pgfault_upcall:
 	        return sys_env_set_pgfault_upcall(a1, (void *)a2);
+	    case SYS_env_set_trapframe:
+	        return sys_env_set_trapframe((envid_t)a1, (struct Trapframe *)a2);
         default:
 	        return -E_INVAL;
 	}
